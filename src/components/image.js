88e6ee9af3b8e6eac5466fb0eacab4dbf56bc2ed
zuhayrhasan <98725504+zuhayrhasan@users.noreply.github.com>
2023-07-25 17:42:25 -0400

    added modal

--- src/components/image.js
+++ src/components/image.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import '../styles/home.css';
+import React, { useState } from 'react';
+import '../styles/image.css';
 
 const Image = ({ person, occasion, imageSrc }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleImageClick = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="column-image">
-      <div className="blur">
-        <div className="blur-person">{person}</div>
-        <div className="blur-occasion">{occasion}</div>
+    <>
+      <div className="column-image" onClick={handleImageClick}>
+        <div className="blur">
+          <div className="blur-person">{person}</div>
+          <div className="blur-occasion">{occasion}</div>
+        </div>
+        <img src={imageSrc} alt="Picture 1" />
       </div>
-      <img src={imageSrc} alt="Picture 1"/>
-    </div>
+
+      {showModal && (
+        <div className="modal-overlay" onClick={handleCloseModal}>
+          <div className="modal">
+            <img src={imageSrc} alt="Picture 1" />
+            <div className="modal-content">
+              <div className="blur-person">{person}</div>
+              <div className="blur-occasion">{occasion}</div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
