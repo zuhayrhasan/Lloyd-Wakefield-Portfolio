104904271aea0d316765ce1ffa9b4797b7a0ee93
zuhayrhasan <98725504+zuhayrhasan@users.noreply.github.com>
2023-07-28 14:57:33 -0400

    clean up nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,7 @@ import '../styles/nav.css'
 
 // Images
 import Logo from '../images/logo.png';
+import Insta from '../images/Insta.png';
 
 
 class NavBar extends Component {
@@ -54,14 +55,16 @@ class NavBar extends Component {
     const contactPos = fullHeight-svh*1.5-footer-safety;
 
     const glider = document.querySelector('.glider');
+    const instaIcon = document.getElementById('insta-icon');
 
     if (isMobile && glider) {
       if (currentScrollY < photosPos) {
         glider.style.transform = 'translateX(-240%)';
+        instaIcon.style.filter = 'brightness(500%)';
       } else if (currentScrollY < cinemaPos) {
-        glider.style.transform = 'translateX(-80%)';
+        glider.style.transform = 'translateX(-82.5%)';
       } else if (currentScrollY < contactPos) {
-        glider.style.transform = 'translateX(80%)';
+        glider.style.transform = 'translateX(77.5%)';
       } else {
         glider.style.transform = 'translateX(240%)';
       }
@@ -73,34 +76,21 @@ class NavBar extends Component {
     const { isNavHidden } = this.state;
     const screenWidth = window.innerWidth; // Is Mobile
 
-    if (screenWidth <= 768) {
-      isNavHidden = false; // No hiding in mobile
-      return (
-        <nav id="navbar-mobile" class={"navbar-mobile" + (isNavHidden ? " mobile-hidden" : "")}>
-          <ul class="nav-list section-tabs">
-            <li><a class="option-mobile" href="#home">H</a></li>
-            <li><a class="option-mobile" href="#photography-section">P</a></li>
-            <li><a class="option-mobile" href="#cinematography-section">V</a></li>
-            <li><a class="option-mobile" href="#contact-section">C</a></li>
-            <span class="glider"/>
-          </ul>
-        </nav>
-      );
-    } 
 
     return (
       <nav id="navbar" class={"navbar" + (isNavHidden ? " hidden" : "")}>
-        <a href="#home">
+        <a href="#home" class="hide-component">
           <div class="logo-circle">
             <img src={Logo} alt="Logo" />
           </div>
         </a>
-        <ul>
-          <li><a class="option" href="#home">Home</a></li>
-          <li><a class="option" href="#photography-section">Photos</a></li>
-          <li><a class="option" href="#cinematography-section">Cinema</a></li>
-          <li><a class="option" href="#contact-section">Contact</a></li>
+        <ul class="nav-list section-tabs">
+          <li><a id="insta-icon" class="option" href="#home" style={{ backgroundImage: `url(${Insta})` }}><span class="tab-text">Home</span></a></li>
+          <li><a id="photos-icon" class="option" href="#photography-section"><span class="tab-text">Photos</span></a></li>
+          <li><a id="video-icon" class="option" href="#cinematography-section"><span class="tab-text">Cinema</span></a></li>
+          <li><a id="contact-icon" class="option" href="#contact-section"><span class="tab-text">Contact</span></a></li>
         </ul>
+        <span class="glider"></span>
       </nav>
     );
 
